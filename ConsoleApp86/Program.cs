9e967c1ebfb245bb215ef714df7d0a376ac8d483
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 15:38:41 +0330

    Add files via upload

---
+++ ConsoleApp86/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp86
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first integer:");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second integer:");
+            int number2 = int.Parse(Console.ReadLine());
+
+
+            if (number1 == number2)
+            {
+                Console.WriteLine($"{number1}=={number2}");
+                Console.ReadLine();
+            }
+            if (number1 != number2)
+            {
+                Console.WriteLine($"{number1}!={number2}");
+                Console.ReadLine();
+            }
+            if (number1 < number2)
+            {
+                Console.WriteLine($"{number1}<{number2}");
+            }
+            if (number1 > number2)
+            {
+                Console.WriteLine($"{number1}>{number2}");
+                Console.ReadLine();
+
+                if (number1 <= number2)
+                {
+                    Console.WriteLine($"{number1}<={number2}");
+                    Console.ReadLine();
+                }
+                if (number1 >= number2)
+                {
+                    Console.WriteLine($"{number1}>={number2}");
+                    Console.ReadLine();
+                }
+                
+
+            }
+
+                }
+
+        }
+    }
+
